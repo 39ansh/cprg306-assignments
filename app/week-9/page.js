a6e2280f41cf8ab409699291d0fdf0639f8ead95
39ansh <141595198+39ansh@users.noreply.github.com>
2024-07-10 21:49:46 -0600

    week-9

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,32 +1,3 @@
-// import React from "react";
-// import { useUserAuth } from "./_utils/auth-context";
-
-// const Page = () => {
-//   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
-
-//   return (
-//     <div style={{ textAlign: "center", marginTop: "50px" }}>
-//       {user ? (
-//         <>
-//           <p>
-//             Welcome, {user.displayName} ({user.email})
-//           </p>
-//           <button onClick={firebaseSignOut}>Logout</button>
-//           <br />
-//           <a href="/shopping-list">Go to Shopping List</a>
-//         </>
-//       ) : (
-//         <button onClick={gitHubSignIn}>Login with GitHub</button>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Page;
-
-
-
-
 "use client";
 import React from "react";
 import { useUserAuth } from "./_utils/auth-context.js";
